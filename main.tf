fa2b635bd965193a39d199e69cbde0911980bfff
DeepaKovvada <118762067+DeepaKovvada@users.noreply.github.com>
2023-01-22 15:11:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 
 resource "azurerm_resource_group" "example" {
-  name     = "vnetrg"
-  location = "West Europe"
+  name     = "vnetrg1"
+  location = "canada central"
 }
 
 
 resource "azurerm_virtual_network" "example" {
-  name                = "vnet1"
+  name                = "vnet01"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   subnet {
-    name           = "subnet1"
+    name           = "subnet01"
     address_prefix = "10.0.1.0/24"
   }
 
   subnet {
-    name           = "subnet2"
+    name           = "subnet02"
     address_prefix = "10.0.2.0/24"
     
   }
